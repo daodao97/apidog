28dca4129b82756594beadb2a98101b83f7b709c
tw2066 <tw2066@163.com>
2020-08-13 14:48:52 +0800

    Update SwaggerJson.php
    对 ApiResponse 下schema 里面的值进行描述

--- src/Swagger/SwaggerJson.php
+++ src/Swagger/SwaggerJson.php
@@ -234,7 +234,9 @@ class SwaggerJson
             return false;
         }
         $definition = [];
-        foreach ($schema as $key => $val) {
+        foreach ($schema as $keyString => $val) {
+            $keyArray =  explode('|',$keyString);
+            $key = $keyArray[0];
             $_key = str_replace('_', '', $key);
             $property = [];
             $property['type'] = gettype($val);
@@ -260,6 +262,7 @@ class SwaggerJson
             } else {
                 $property['default'] = $val;
             }
+            $property['description'] = $keyArray[1] ?? '';
             $definition['properties'][$key] = $property;
         }
         if ($level === 0) {
